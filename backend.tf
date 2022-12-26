75bb099d0fa2f14debfbac2f44fabae5f9cc4b12
mohini <phatakmohini@gmail.com>
2022-12-26 20:02:53 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-jenkins-state-bucket-moni123"
+    bucket = "my-aws-tf-state-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
