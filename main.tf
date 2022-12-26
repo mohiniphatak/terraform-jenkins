f2b3aaa3406e18685681b67f5c94e37a9c4d6d0e
mohini <phatakmohini@gmail.com>
2022-12-26 19:28:49 +0530

    updated

--- main.tf
+++ main.tf
@@ -33,17 +33,6 @@ provider "aws" {
 # }
 
 
-
-resource "aws_s3_bucket_public_access_block" "access" {
-  bucket = aws_s3_bucket.s3-sink.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-
 # resource "aws_s3_bucket_notification" "bucket_notification" {
 #   bucket = aws_s3_bucket.b.id
 
