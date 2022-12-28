e1d1b6f9b2affb0ad20dff8865ea6ced845f5b40
mohini <phatakmohini@gmail.com>
2022-12-28 10:26:44 +0530

    jghg

--- backend.tf
+++ backend.tf
@@ -1,7 +1,21 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
   backend "s3" {
     bucket = "terraform-jenkins-state-bucket-moni123"
-    key = "main"
+    key = "stateinfo.state"
     region = "ap-south-1"
     dynamodb_table = "my-dynamo-db-table"
   }
--- variables.tf
+++ variables.tf
@@ -4,6 +4,11 @@
 #   type = string
 # }
 
+variable "aws_region" {
+  type = string
+  default = "ap-south-1"
+}
+
 variable "env" {
   type = string
   default	= "Test"
