a4e19eab5c7b9ac73a4e25c61a46130b9e23a888
mohini <phatakmohini@gmail.com>
2022-12-28 10:25:21 +0530

    upda

--- main.tf
+++ main.tf
@@ -16,19 +16,7 @@
 #   region  = "ap-south-1"
 # }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-}
 
 # # Resource Block
 # resource "aws_instance" "ec2demotest" {
