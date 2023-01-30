d842ca0ab4624b72d6cbbb603fc45cbe76611bb6
mohini <phatakmohini@gmail.com>
2023-01-30 15:32:39 +0530

    docker deployment

--- S3.tf
+++ S3.tf
@@ -2,18 +2,18 @@ resource "aws_s3_bucket" "s3-sink" {
  bucket = "bucket-created-jenkins-terraform"
 }
 
-# resource "aws_s3_bucket" "s3-buck" {
-#  bucket = "bucket-created-jenkins-terraform-again-again"
-# }
+resource "aws_s3_bucket" "s3-buck" {
+ bucket = "bucket-created-jenkins-terraform-again-again"
+}
 
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.s3-sink.id
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.s3-sink.id
 
-#   queue {
-#     queue_arn     = aws_sqs_queue.terraform_queue.arn
-#     events        = ["s3:ObjectCreated:*"]
-#   }
-# }
+  queue {
+    queue_arn     = aws_sqs_queue.terraform_queue.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
 
 
 
